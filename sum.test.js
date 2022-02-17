364b7ea16205987f01ba3f1206810ea0d4ae1426
Parvathy Hari <parvathyvhari@gmail.com>
2022-02-17 21:39:52 +0530

    feat: added configuration file for jest-html-reporter

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,19 @@
-const sum = require('./sum')
+const sum = require('./sum');
 
 test('add two numbers', () => {
-    expect(sum(1, 2)).toBe(3)
-})
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('adding two numbers', () => {
+    expect(sum(0, 2)).toBe(2);
+});
+
+test('add zeros', () => {
+    expect(sum(0, 0)).toBe(0);
+});
+
+test('add two negative numbers', () => {
+    expect(sum(-3, -2)).toBe(-5);
+});
+
+
